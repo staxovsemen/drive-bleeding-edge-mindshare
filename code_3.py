ae836b4b53faf08f7769b2220be052e535ccdd5e
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-09-21 19:20:36 -0400

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+    });
+    data.forEach(item => {
+
+main();
+    return data;
+}
+
+    }
+}
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+
+function generateRandomData() {
+function main() {
