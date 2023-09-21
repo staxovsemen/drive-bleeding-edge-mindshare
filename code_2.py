ee75b2ce308da5fde4821669c451ad39deed2c1c
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-09-21 19:20:36 -0400

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    main()
+if __name__ == "__main__":
+def generate_random_data():
+import random
+    data = generate_random_data()
+def main():
+
+    for item in data:
+        print(f"Random Number: {item}")
